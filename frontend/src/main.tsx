0c2ba6a6ce4d36bc582ebeb5861a19e90bd547aa
ArshAli911 <co23313@ccet.ac.in>
2026-04-14 12:36:30 +0530

    Refactor and split CSS architecture

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import App from "./components/App";
 import "./style/index.css";
+import "./style/toolbar.css";
+import "./style/canvas.css";
+import "./style/inspector.css";
+import "./style/finding.css";
+import "./style/modal.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
